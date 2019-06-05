16004b266696467666c27c382ac3bcd9ef75a574
txoof <aaron.ciuffo@gmail.com>
2019-06-05 19:48:24 +0200

    add find and copy

--- insertFiles.zsh
+++ insertFiles.zsh
@@ -2,21 +2,21 @@
 # written by Aaron Ciuffo (aaron.ciuffo@gmail.com)
 # https://github.com/txoof/insertFiles
 
-# add test results into Google Drive portfolio folders via google filestream
+# add test results into Google Drive portfolio folders over google filestream
 
 # update this if the name of the shared drive changes
-mySharedDrive="/Volumes/GoogleDrive/Shared\ drives/ASH\ Student\ Cumulative\ Folders/"
+mySharedDrive="/Volumes/GoogleDrive/Shared drives/ASH Student Cumulative Folders/"
 
 checkFile="checkFile_DO_NOT_REMOVE.txt"
 
 myLongName='com.txoof.'`basename $0`
 myName=`basename "$0"`
 
-curMonth=`date '+%m'`
-curYear=`date '+%Y'`
 
 schoolYear() {
   # return the school year string based on the current date
+  curMonth=`date '+%m'`
+  curYear=`date '+%Y'`
   if [[ $curMonth -ge 1 ]] && [[ $curMonth -le 07 ]]; then
     endYear=$curYear
     startYear=$((curYear-1))
@@ -24,11 +24,11 @@ schoolYear() {
     endYear=$((curYear+1))
     startYear=$curYear
   fi
-  echo "SY_$startYear-$endYear"
+  # return string with sy value
+  echo "SY$startYear-$endYear"
 }
 
-# main
-if [[ $# -lt 1 ]]; then
+usage() {
   echo "$myName: inserts multiple test files into a Google Shared drive" 
   echo "folder based on student numbers"
   echo "=========================="
@@ -41,13 +41,36 @@ if [[ $# -lt 1 ]]; then
   echo "written by Aaron Ciuffo - aaron.ciuffo@gmail.com"
   echo "updates at: https://github.com/txoof/insertFiles"
   exit 0
+
+}
+
+
+
+
+if [[ $# -lt 1 ]]; then
+  usage
 fi
 
+# create a directory cache of folders stored in sharedDrive
+myTempDir=$TMPDIR${myLongName}
+dirCache=$myTempDir/sharedDriveCache.txt
+
+if [[ ! -d $myTempDir ]]; then
+  mkdir $myTempDir
+fi
+
+find "${mySharedDrive}" -maxdepth 3 -type d > $dirCache
+
+# set the current school year
+mySchoolYear=`schoolYear`
+
 # loop through each file provided on the command line
-for each in "${@}" 
+for each in "${@}"
 do
   # extract the student number from each file
   stuNumber=`echo ${each} | sed 's/.*[^0-9]\([0-9]\{5,10\}\).*\.pdf$/\1/g'`
-  
+  studentDir=`grep $stuNumber "${dirCache}"`
+  newName=${mySchoolYear}_`basename $each`
+  echo copy ${each} ${studentDir}/${newName}
 done
 
