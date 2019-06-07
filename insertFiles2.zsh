b35823e17a9b68b5f986481af11b8632cd81dea4
txoof <aaron.ciuffo@gmail.com>
2019-06-07 13:27:53 +0200

    work on grepping

--- insertFiles2.zsh
+++ insertFiles2.zsh
@@ -1,9 +1,14 @@
 #!/bin/zsh
 # set the shared drive name for the portfolio folder
 mySharedDriveName="ASH Student Cumulative Folders"
+# the name of the folder(s) that contains all of the cum folders
+# this should be relative to mySharedDrive
+myCumFolder="Student Cumulative Folders (AKA Student Portfolios)"
 
 # root of Google Shared Drives
 mySharedRoot="/Volumes/GoogleDrive/Shared drives/"
+
+
 
 # TODO fix all function variables so they are local variables
 
@@ -11,11 +16,11 @@ mySharedRoot="/Volumes/GoogleDrive/Shared drives/"
 # Functions
 schoolYear() {
   # return the school year string based on the current date
-  curMonth=`date '+%m'`
-  curYear=`date '+%Y'`
+  local curMonth=`date '+%m'`
+  local curYear=`date '+%Y'`
   if [[ $curMonth -ge 1 ]] && [[ $curMonth -le 07 ]]; then
-    endYear=$curYear
-    startYear=$((curYear-1))
+    local endYear=$curYear
+    local startYear=$((curYear-1))
   else
     endYear=$((curYear+1))
     startYear=$curYear
@@ -24,8 +29,7 @@ schoolYear() {
   echo "SY$startYear-$endYear"
 }
 
-usage() {
-  
+usage() { 
   # usage instructions
   if [[ ${#[@]} -lt 1 ]]; then
     printf "$myName inserts multiple test files into a Google Shared drive
@@ -37,6 +41,7 @@ $myName [--<grade sub-folder>] /path/to/files/.*
 
 Grade Sub Folders: --ps, --tk, --kg, --1..--12
 
+EXAMPLE:
 Insert all PDFs in ~/Downloads/grade3 into 03-Grade sub folders
 $] $myName --3 ~/Downloads/grade3/*.pdf
 
@@ -51,7 +56,7 @@ updates at: https://github.com/txoof/insertFiles"
 checkSentry() {
   # check for sentry file on Google Shared drive
   # exit out if sentry file is missing
-  if [[ ! -f $mySharedDrive/$sentryFile ]]; then
+  if [[ ! -f $mySharedRoot/$mySharedDriveName/$sentryFile ]]; then
 printf "FATAL ERROR
 Sentry file is missing from $mySharedDriveName
 
@@ -81,7 +86,7 @@ cacheDirs() {
   checkSentry
 
   # create the cache file
-  find "${mySharedDrive}" -maxdepth 4 -type d > $driveCache
+  find "${mySharedDrive}" -maxdepth 2 -type d > $driveCache
   if [[ $? -gt 0 ]]; then
     echo "caching of remote directories failed"
     echo "See $driveCache for more details"
@@ -92,45 +97,74 @@ cacheDirs() {
 
 parseArgs() {
   # parse the command line arguments
-  argOne=$1
-  regexp="--([0-9]+)"
+  local argOne=$1
+  local regexp="--([0-9]+)"
 
-  gradeFolders=(00-Preschool 00-Transition 00-zKindergarten 01-Grade 02-Grade 03-Grade 04-Grade 05-Grade 06-Grade 07-Grade 08-Grade 09-Grade 10-Grade 11-Grade 12-Grade)
+  local gradeFolders=(00-Preschool 00-Transition 00-zKindergarten 01-Grade 02-Grade 03-Grade 04-Grade 05-Grade 06-Grade 07-Grade 08-Grade 09-Grade 10-Grade 11-Grade 12-Grade)
 
   # check for an a grade level argument (--ps, --tk, --kg, --1, --12)
   [[ $argOne =~ $regexp ]]
-  gradeLevel=${match[1]}
-  gradeIndex=$((gradeLevel+3))
+  local gradeLevel=${match[1]}
+  local gradeIndex=$((gradeLevel+3))
 
   case $argOne in
     --ps)
-      gradeFolder=$gradeFolders[1]
+      local gradeFolder=$gradeFolders[1]
       ;;
     --tk)
-      gradeFolder=$gradeFolders[2]
+      local gradeFolder=$gradeFolders[2]
       ;;
     --kg)
-      gradeFolder=$gradeFolders[3]
+      local gradeFolder=$gradeFolders[3]
       ;;
     --1|--2|--3|--4|--5|--6|--7|--8|--9|--10|--11|--12)
-      gradeFolder=$gradeFolders[$gradeIndex]
+      local gradeFolder=$gradeFolders[$gradeIndex]
       ;;
     *)
+      printf "Unknown option: $argOne\n\n"
       usage
   esac
 
   echo $gradeFolder
 }
 
+execute() {
+  "$@"
+  local mystat=$?
+  if [ $mystat -ne 0 ]; then
+    echo "error with $1" >&2
+  fi
+  echo $mystat
+}
+
 insertFiles() {
-  regex=".*?(\d{5,}).*"
+  local failCopy=()
+  local failCache=()
+  local failFileName=()
+  local success=()
   for each in "${fileArgs[@]}"
   do
-    # extract student number from each file
-    [[ $each =~ $regex ]]
-    stuNum=${match[1]}
-    echo $stuNum
+    local match=()
+    # capture 5 or more consecutive digits in supplied ilename
+    [[ $each =~ "([0-9]{5,})" ]]
+    if [[ ${#match[1]} -gt 0 ]]; then
+      local stuNum=${match[1]}
+      # grep and attempt to insert file into path here
+      studentDir=$(exec grep "$stuNum" "${driveCache}")
+      echo %%%%%%%%%%%%%%%
+      echo $studentDir
+      if [[ $? -gt 0 ]]; then
+        failCache+=$each
+        continue
+      fi
+
+      #echo cp $each to $studentDir
+
+    else
+      failFileName+=$each
+    fi
   done
+
 }
 ##################################
 # main
@@ -139,17 +173,17 @@ insertFiles() {
 [[ `basename ${0}` =~ "(^.*)\..*" ]]
 myName=${match[1]}
 myLongName="com.txoof."${myName}
-mySharedDrive=${mySharedRoot}/${mySharedDriveName}/
+mySharedDrive=${mySharedRoot}/${mySharedDriveName}/${myCumFolder}
 
 # check for arguments - give usage instructions if no arguments
 usage $@
 
 # if gradelevel switch is provided, set a sub folder within the student folder
 gradeFolder=$(parseArgs $1)
-echo gradeFolder $gradeFolder
+
 # discard switches; keep file arguments
 if [[ $1 =~ "^--.*" ]]; then
-  fileArgs=${@:2}
+  fileArgs=(${@:2})
 else
   fileArgs=($@)
 fi
@@ -162,12 +196,14 @@ fi
 
 # set temp directory for cache
 myTempDir=$TMPDIR${myLongName}
-#driveCache=$myTempDir/sharedDriveCache.txt
+driveCache=$myTempDir/sharedDriveCache.txt
 # sentry file that indicates this is the proper Shared drive
 # if file is missing, bail out
 sentryFile="sentryFile_DO_NOT_REMOVE.txt"
 
 # cache the files on the shared drive for faster searching
-# cacheDirs
+cacheDirs
 
-#insertFiles
+insertFiles $fileArgs
+
+
