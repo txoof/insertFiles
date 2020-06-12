40380ee9c05f09e5bb8e79311ac852c686ce4420
txoof <aaron.ciuffo@gmail.com>
2020-06-12 12:55:17 +0200

    add kg opiton in gradelevel select

--- package.sh
+++ package.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+developmentApp="InsertFiles_development.app"
+deployApp="insertFiles.app"
 packageName="insertFiles.app.zip"
+cp -R $developmentApp $deployApp
 zip -r $packageName ./insertFiles.app
 git commit -m "refresh distribution package" $packageName
 git push
