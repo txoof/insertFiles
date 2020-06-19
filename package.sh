a2387f51f318d5ac5f71fd6c9531a766de8e5e9d
txoof <aaron.ciuffo@gmail.com>
2020-06-19 08:12:13 +0200

    fix zip file creation

--- package.sh
+++ package.sh
@@ -3,6 +3,6 @@ developmentApp="InsertFiles_development.app"
 deployApp="insertFiles.app"
 packageName="insertFiles.app.zip"
 cp -R $developmentApp $deployApp
-zip -r $packageName ./insertFiles.app
+zip -r $packageName $deployApp 
 git commit -m "refresh distribution package" $packageName
 git push
