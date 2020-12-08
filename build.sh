e6173a5cb945d9b4c21b521f3bdeffe61ccd89e2
txoof <aaron.ciuffo@gmail.com>
2020-12-08 10:51:51 +0100

    update build scripts to automatically update version numbers

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/zsh
 app_name='insert_files'
+source_path=./insert_files
+version_number=$(grep VERSION $source_path/constants.py | sed -nE  's/^VERSION[ ]+=[ ]+(.*)/\1/p' | tr -d \'\")
 
-pushd ./insert_files
-jupyter-nbconvert --to python --template python_clean insert_files.ipynb
+pushd $source_path
+echo "runing nbconvert on $app_name.ipynb"
+jupyter-nbconvert --to python --template python_clean $app_name.ipynb
+echo "building with pyinstaller"
 pipenv run pyinstaller --onefile --noconfirm --clean --add-data insert_files.ini:. --add-data logging_cfg.ini:. --add-data Help.md:. --exclude-module IPython $app_name.py
 #pushd ./dist
 #tar cvzf  ../../$app_name.tgz ./$app_name
-#popd
-~/bin/develtools/pycodesign.py insert_files_codesign.ini
+popd
+echo "code signing"
+~/bin/develtools/pycodesign.py -O $version_number insert_files_codesign.ini
 popd
--- publish.sh
+++ publish.sh
@@ -4,10 +4,14 @@ source_path=./insert_files
 #publish_tar=insert_files.tgz
 publish_pkg=insert_files.pkg
 
-version_number=`grep VERSION $source_path/constants.py | sed -nE  's/^VERSION[ ]+=[ ]+(.*)/\1/p' | tr -d \'\"`
+version_number=$(grep VERSION $source_path/constants.py | sed -nE  's/^VERSION[ ]+=[ ]+(.*)/\1/p' | tr -d \'\")
+
 
 
 if [ -z "$1" ]; then
+  echo $0 updates the readme with current version number and pushes
+  echo and tags a release of $publish_pkg to github
+  echo " " 
   echo current version number: v$version_number
   echo tar build, tag and push release to github
   echo usage:
@@ -15,16 +19,27 @@ if [ -z "$1" ]; then
   exit 0
 fi
 
-# run the build script
-#./build.sh
+echo "updating README.md"
+
+if ! command -v /usr/local/bin/gsed
+then
+  echo "gnu-sed must be installed for this script to function properly:"
+  echo "$ brew install gnu-sed"
+  exit 1
+else
+  gsed -i "s/\(## Current Version:\).*/\1 $version_number/g" README.md
+  gsed -i "s/^M//g" README.md
+  git commit -m "update version " README.md
+fi
 
 tag="v$version_number"
 
 pycodesign.py -O $version_number codesign.ini
 
-if [ $? -ne 0 ]: then
+if [ $? -ne 0 ] 
+then
   echo codesigning failed, exiting
-  exit
+  exit 1
 fi
 
 git tag -a "$tag" -m "$1"
